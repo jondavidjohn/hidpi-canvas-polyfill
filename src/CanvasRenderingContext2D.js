39342e1d691527b666271cd76d3a18e1e650e7a1
Jonathan Johnson <jon@crowdfavorite.com>
2013-10-23 09:16:38 -0600

    abstract out general use of *= ratio of arguments
      - Make function list declaritive

--- src/CanvasRenderingContext2D.js
+++ src/CanvasRenderingContext2D.js
@@ -1,98 +1,58 @@
 (function(prototype) {
 
 	var getPixelRatio = function(context) {
-		var backingStore = context.backingStorePixelRatio ||
-					context.webkitBackingtorePixelRatio ||
-					context.mozBackingStorePixelRatio ||
-					context.msBackingStorePixelRatio ||
-					context.oBackingStorePixelRatio ||
-					context.backingStorePixelRatio || 1;
+			var backingStore = context.backingStorePixelRatio ||
+						context.webkitBackingtorePixelRatio ||
+						context.mozBackingStorePixelRatio ||
+						context.msBackingStorePixelRatio ||
+						context.oBackingStorePixelRatio ||
+						context.backingStorePixelRatio || 1;
 
-		return (window.devicePixelRatio || 1) / backingStore;
-	};
+			return (window.devicePixelRatio || 1) / backingStore;
+		},
+		ratioArgs = {
+			'fillRect': 'all',
+			'clearRect': 'all',
+			'strokeRect': 'all',
+			'moveTo': 'all',
+			'lineTo': 'all',
+			'arc': [0,1,2],
+			'arcTo': 'all',
+			'bezierCurveTo': 'all',
+			'isPointinPath': 'all',
+			'isPointinStroke': 'all',
+			'quadraticCurveTo': 'all',
+			'rect': 'all',
+			'translate': 'all'
+		},
+		func, value;
 
-	// Rect
-	//
+		for (func in ratioArgs) {
+			if (ratioArgs.hasOwnProperty(func)) {
+				(function(value) {
+					prototype[func] = (function(_super) {
+						return function() {
+							var ratio = getPixelRatio(this), i, len,
+								args = Array.prototype.slice.call(arguments);
 
-	prototype.fillRect = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
+							if (value === 'all') {
+								return _super.apply(this, args.map(function(a) { return a * ratio; }));
+							}
+							else if (Object.prototype.toString.call(value) == '[object Array]') {
+								for (i = 0, len = value.length; i < len; i++) {
+									args[value[i]] *= ratio;
+								}
+								return _super.apply(this, args);
+							}
+						};
+					})(prototype[func]);
+				})(ratioArgs[func]);
+			}
+		}
 
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.fillRect);
-
-	prototype.clearRect = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.clearRect);
-
-	prototype.strokeRect = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.strokeRect);
-
-	// Lines
-	//
-
-	prototype.moveTo = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.moveTo);
-
-	prototype.lineTo = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.lineTo);
-
-	// Arcs
-	//
-
-	prototype.arc = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			args[0] *= ratio; // x
-			args[1] *= ratio; // y
-			args[2] *= ratio; // radius
-
-			return _super.apply(this, args);
-		};
-	})(prototype.arc);
-
-	// Bezier Curves
-	//
-
-	prototype.bezierCurveTo = (function(_super) {
-		return function() {
-			var ratio = getPixelRatio(this),
-				args = Array.prototype.slice.call(arguments);
-
-			return _super.apply(this, args.map(function(a) { return a * ratio; }));
-		};
-	})(prototype.bezierCurveTo);
 
 	// Text
 	//
-
 	prototype.fillText = (function(_super) {
 		return function() {
 			var ratio = getPixelRatio(this),
@@ -108,4 +68,20 @@
 			return _super.apply(this, args);
 		};
 	})(prototype.fillText);
+
+	prototype.strokeText = (function(_super) {
+		return function() {
+			var ratio = getPixelRatio(this),
+				args = Array.prototype.slice.call(arguments);
+
+			args[1] *= ratio; // x
+			args[2] *= ratio; // y
+
+			this.font = this.font.replace(/(\d+)(px|em|rem|pt)/g, function(w, m, u) {
+				return (m * ratio) + u;
+			});
+
+			return _super.apply(this, args);
+		};
+	})(prototype.strokeText);
 })(CanvasRenderingContext2D.prototype);
