61421f7134ebcc0f4a6e423acd5ded2d05256f43
Jonathan Johnson <jon@crowdfavorite.com>
2014-01-30 22:28:24 -0600

    add tests for font scaling and canvas scaling

--- test/CanvasRenderingContext2DTests.js
+++ test/CanvasRenderingContext2DTests.js
@@ -1,5 +1,19 @@
-module('CanvasRenderingContext2D Tests');
+module('CanvasRenderingContext2D Tests', {
+	setup: function() {
+		window.devicePixelRatio = 2;
+	}
+});
 
-test('something', function() {
-	equal(true, true);
+test('the font size remains unchanged after every text call', function() {
+	var font_value = '12px Helvetica',
+		canvas = document.getElementById('test_canvas'),
+		context = canvas.getContext('2d');
+
+	context.font = font_value;
+
+	context.fillText("Some Text",10,300);
+	equal(context.font, font_value);
+
+	context.fillText("Some More Text",10,450);
+	equal(context.font, font_value);
 });
