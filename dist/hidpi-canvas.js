97814ab45a0aa08ff1d08d4aaf2823f862616522
Jonathan Johnson <me@jondavidjohn.com>
2014-03-25 09:58:12 -0600

    add createLinearGradient (Closes #6)

--- dist/hidpi-canvas.js
+++ dist/hidpi-canvas.js
@@ -43,7 +43,8 @@
 			'quadraticCurveTo': 'all',
 			'rect': 'all',
 			'translate': 'all',
-			'createRadialGradient': 'all'
+			'createRadialGradient': 'all',
+			'createLinearGradient': 'all'
 		};
 
 	forEach(ratioArgs, function(value, key) {
