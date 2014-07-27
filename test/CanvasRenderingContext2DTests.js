d72cdd97366ac5e1dbadd3038f549f296e9770be
Jonathan Johnson <me@jondavidjohn.com>
2014-07-27 12:05:04 -0500

    Handle lineWidth ratioing within stroke calls (fixes #14)

--- test/CanvasRenderingContext2DTests.js
+++ test/CanvasRenderingContext2DTests.js
@@ -17,3 +17,21 @@ test('the font size remains unchanged after every text call', function() {
 	context.fillText("Some More Text",10,450);
 	equal(context.font, font_value);
 });
+
+test('the lineWidth property remains unchanged after every stroke call', function() {
+	var line_width_value = 12,
+		canvas = document.getElementById('test_canvas'),
+		context = canvas.getContext('2d');
+
+	context.lineWidth = line_width_value;
+
+	context.moveTo(250,150);
+	context.arcTo(350,50,350,100,50);
+	context.stroke();
+	equal(context.lineWidth, line_width_value);
+
+	context.moveTo(10,15);
+	context.arcTo(30,50,30,10,50);
+	context.stroke();
+	equal(context.lineWidth, line_width_value);
+});
